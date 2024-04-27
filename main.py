681f1979bac3224e324666418a2f824b742ab984
karanjaE <rranchow@gmail.com>
2024-04-27 19:08:15 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from tortoise.contrib .fastapi import register_tortoise
+from models import *
+
+app = FastAPI()
+
+@app.get("/")
+def index():
+    return {"message": "Hola!"}
+
+register_tortoise(
+    app, 
+    db_url="sqlite://database.sqlite3",
+    models={"models": ["models"]},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
